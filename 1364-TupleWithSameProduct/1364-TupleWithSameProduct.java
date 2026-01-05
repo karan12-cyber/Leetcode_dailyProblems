9655a9a58a50b167b16f5d7bf64383fefcec2f57
karan dixit <153681323+karan12-cyber@users.noreply.github.com>
2026-01-05 23:24:19 +0530

    Create 1364-TupleWithSameProduct

---
+++ 1364-TupleWithSameProduct/1364-TupleWithSameProduct.java
@@ -0,0 +1,18 @@
+// Last updated: 1/5/2026, 11:24:19 PM
+import java.util.HashMap;
+
+class Solution {
+    public int tupleSameProduct(int[] nums) {
+        HashMap<Integer, Integer> productCount = new HashMap<>();
+        int result = 0;
+
+        for (int i = 0; i < nums.length; i++)
+            for (int j = i+1; j < nums.length; j++) {
+                int product = nums[i] * nums[j];
+                result += 8 * productCount.getOrDefault(product, 0);
+                productCount.put(product, productCount.getOrDefault(product, 0) + 1);
+            }
+
+        return result;
+    }
+}
