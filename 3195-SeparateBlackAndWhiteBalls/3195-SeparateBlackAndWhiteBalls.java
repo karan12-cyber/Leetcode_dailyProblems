32928c532b835a3ac4a4cd9e67f8d6b7b7ac832f
karan dixit <153681323+karan12-cyber@users.noreply.github.com>
2026-01-05 23:23:03 +0530

    Create 3195-SeparateBlackAndWhiteBalls

---
+++ 3195-SeparateBlackAndWhiteBalls/3195-SeparateBlackAndWhiteBalls.java
@@ -0,0 +1,14 @@
+// Last updated: 1/5/2026, 11:23:03 PM
+class Solution {
+    public long minimumSteps(String s) {
+        long swap = 0;
+        int black = 0; 
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0') 
+                swap += (long) black; 
+            else
+                black++; 
+        }
+        return swap;
+    }
+}
