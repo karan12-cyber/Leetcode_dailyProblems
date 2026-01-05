579518141a744a2f3cb3162cd69351b1a007c0f9
karan dixit <153681323+karan12-cyber@users.noreply.github.com>
2026-01-05 23:23:55 +0530

    Create 1915-CheckIfOneStringSwapCanMakeStringsEqual

---
+++ 1915-CheckIfOneStringSwapCanMakeStringsEqual/1915-CheckIfOneStringSwapCanMakeStringsEqual.java
@@ -0,0 +1,23 @@
+// Last updated: 1/5/2026, 11:23:55 PM
+class Solution {
+    public boolean areAlmostEqual(String s1, String s2) {
+        int i=-1, j=-1;
+        int cnt=0;
+
+        for(int k=0; k<s1.length(); k++){
+            if(s1.charAt(k)!=s2.charAt(k)){
+                cnt++;
+                if(i==-1) i=k;
+                else if(j==-1) j=k;
+            }
+        }
+
+        if(cnt==0) return true;
+        else if(cnt==2 && s1.charAt(i)==s2.charAt(j) && s1.charAt(j)==s2.charAt(i)){
+            return true;
+        }
+
+
+        return false;
+    }
+}
