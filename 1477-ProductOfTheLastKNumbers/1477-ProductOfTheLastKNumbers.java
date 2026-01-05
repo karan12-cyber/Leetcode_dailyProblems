788a6b6c368b3d6557a5239c5bf96d3edb340827
karan dixit <153681323+karan12-cyber@users.noreply.github.com>
2026-01-05 23:24:12 +0530

    Create 1477-ProductOfTheLastKNumbers

---
+++ 1477-ProductOfTheLastKNumbers/1477-ProductOfTheLastKNumbers.java
@@ -0,0 +1,26 @@
+// Last updated: 1/5/2026, 11:24:13 PM
+class ProductOfNumbers {
+    ArrayList<Integer> list = new ArrayList<>();
+    int prod = 1;
+
+    public ProductOfNumbers() {
+        
+    }
+    
+    public void add(int num) {
+        if(num == 0){
+            list = new ArrayList<>();
+            prod = 1;
+            return;
+        }
+        prod = prod * num;
+        list.add(prod);
+    }
+    
+    public int getProduct(int k) {
+        if(list.size() < k) return 0;
+        int ans = list.get(list.size() - 1);
+        if(list.size() == k) return ans;
+        return ans / list.get(list.size() - 1 - k);
+    }
+}
