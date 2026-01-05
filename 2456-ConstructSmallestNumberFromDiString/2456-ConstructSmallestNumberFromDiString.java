f7757bea4e8b2e9c644b621494a1546695bd33fc
karan dixit <153681323+karan12-cyber@users.noreply.github.com>
2026-01-05 23:23:34 +0530

    Create 2456-ConstructSmallestNumberFromDiString

---
+++ 2456-ConstructSmallestNumberFromDiString/2456-ConstructSmallestNumberFromDiString.java
@@ -0,0 +1,21 @@
+// Last updated: 1/5/2026, 11:23:34 PM
+class Solution {
+    public String smallestNumber(String pattern) {
+        int n = pattern.length();
+        StringBuilder result = new StringBuilder();
+        int[] stack = new int[n + 1]; 
+        int index = 0;
+
+        for (int i = 0; i <= n; i++) {
+            stack[index++] = i + 1;
+
+            if (i == n || pattern.charAt(i) == 'I') {
+                while (index > 0) {
+                    result.append(stack[--index]);
+                }
+            }
+        }
+
+        return result.toString();
+    }
+}
