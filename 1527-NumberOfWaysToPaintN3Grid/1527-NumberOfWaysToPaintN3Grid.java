874d7001ef166ff017cfb64ccf96b2b66e2aab7e
karan dixit <153681323+karan12-cyber@users.noreply.github.com>
2026-01-05 23:24:10 +0530

    Create 1527-NumberOfWaysToPaintN3Grid

---
+++ 1527-NumberOfWaysToPaintN3Grid/1527-NumberOfWaysToPaintN3Grid.java
@@ -0,0 +1,19 @@
+// Last updated: 1/5/2026, 11:24:10 PM
+class Solution {
+    public int numOfWays(int n) {
+        final int MOD = 1_000_000_007;
+
+        long same = 6;
+        long diff = 6;
+
+        for (int i = 2; i <= n; i++) {
+            long newSame = (same * 3 + diff * 2) % MOD;
+            long newDiff = (same * 2 + diff * 2) % MOD;
+
+            same = newSame;
+            diff = newDiff;
+        }
+
+        return (int)((same + diff) % MOD);
+    }
+}
