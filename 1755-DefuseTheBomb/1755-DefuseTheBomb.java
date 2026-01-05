d5d1ad93e78bcc481dad82aef3e6aa298465df2b
karan dixit <153681323+karan12-cyber@users.noreply.github.com>
2026-01-05 23:24:03 +0530

    Create 1755-DefuseTheBomb

---
+++ 1755-DefuseTheBomb/1755-DefuseTheBomb.java
@@ -0,0 +1,21 @@
+// Last updated: 1/5/2026, 11:24:03 PM
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int N = code.length;
+        int[] res = new int[N];
+        
+        for (int i = 0; i < N; i++) {
+            if (k > 0) {
+                for (int j = i + 1; j < i + 1 + k; j++) {
+                    res[i] += code[j % N];
+                }
+            } else if (k < 0) {
+                for (int j = i - 1; j > i - 1 - Math.abs(k); j--) {
+                    res[i] += code[((j % N) + N) % N];
+                }
+            }
+        }
+        
+        return res;
+    }
+}
