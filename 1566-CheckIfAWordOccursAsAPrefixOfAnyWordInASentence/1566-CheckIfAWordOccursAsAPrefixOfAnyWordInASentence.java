ec0a1f254f77c4019f1cfd350c8e4d46534458a9
karan dixit <153681323+karan12-cyber@users.noreply.github.com>
2026-01-05 23:24:09 +0530

    Create 1566-CheckIfAWordOccursAsAPrefixOfAnyWordInASentence

---
+++ 1566-CheckIfAWordOccursAsAPrefixOfAnyWordInASentence/1566-CheckIfAWordOccursAsAPrefixOfAnyWordInASentence.java
@@ -0,0 +1,12 @@
+// Last updated: 1/5/2026, 11:24:09 PM
+class Solution {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        String[] words = sentence.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].startsWith(searchWord)) {
+                return i + 1;
+            }
+        }
+        return -1;
+    }
+}
