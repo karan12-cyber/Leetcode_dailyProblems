6e2c9755e936ecbe5ebdf5b9c175fbffa86cf300
karan dixit <153681323+karan12-cyber@users.noreply.github.com>
2026-01-05 23:24:54 +0530

    Create 234-PalindromeLinkedList

---
+++ 234-PalindromeLinkedList/234-PalindromeLinkedList.java
@@ -0,0 +1,16 @@
+// Last updated: 1/5/2026, 11:24:55 PM
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        Stack<Integer> stack = new Stack();
+        ListNode curr = head;
+        while(curr != null) {
+            stack.push(curr.val);
+            curr = curr.next;
+        }
+        curr = head;
+        while(curr != null && curr.val == stack.pop()) {
+            curr = curr.next;
+        }
+        return curr == null;
+    }
+}
