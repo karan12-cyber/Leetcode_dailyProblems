5b5572abe88dfca34fbd1f022029dbc322695381
karan dixit <153681323+karan12-cyber@users.noreply.github.com>
2026-06-29 09:12:26 +0530

    File updated at 6/29/2026, 9:12:24 AM

--- 682-BaseballGame/682-BaseballGame.java
+++ 682-BaseballGame/682-BaseballGame.java
@@ -1,24 +1,24 @@
-// Last updated: 1/5/2026, 11:24:48 PM
-class Solution {
-    public int calPoints(String[] operations) {
-        Stack<Integer> stack = new Stack<>();
-        int sum  = 0;
-        for (String op : operations) {
-            if (op.equals("C")) {
-                stack.pop(); 
-            } else if (op.equals("D")) {
-                stack.push(stack.peek() * 2);
-            } else if (op.equals("+")) {
-                int top = stack.pop();
-                int newTop = top + stack.peek();
-                stack.push(top);
-                stack.push(newTop);
-            } else {
-                stack.push(Integer.parseInt(op));
-            }
-        }
-
-        for(int k : stack) sum+=k;
-        return sum;
-    } 
-}
+// Last updated: 6/29/2026, 9:12:24 AM
+1class Solution {
+2    public int calPoints(String[] operations) {
+3        Stack<Integer> stack = new Stack<>();
+4        int sum  = 0;
+5        for (String op : operations) {
+6            if (op.equals("C")) {
+7                stack.pop(); 
+8            } else if (op.equals("D")) {
+9                stack.push(stack.peek() * 2);
+10            } else if (op.equals("+")) {
+11                int top = stack.pop();
+12                int newTop = top + stack.peek();
+13                stack.push(top);
+14                stack.push(newTop);
+15            } else {
+16                stack.push(Integer.parseInt(op));
+17            }
+18        }
+19
+20        for(int k : stack) sum+=k;
+21        return sum;
+22    } 
+23}
